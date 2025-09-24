33add2642d9ff1c998aad74f756f4d2a56e73b95
joindatasolutions-lab <joindatasolutions@gmail.com>
2025-09-24 11:06:44 -0500

    Move site to docs/flora_pipeline (GitHub Pages)

--- flora_pipeline/docs/flora_pipeline/flutter_service_worker.js
+++ flora_pipeline/docs/flora_pipeline/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f9847c2b9c2b4401d89c1d816d9ed750
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d8a68d828ef8e757a574eae5e811fb1b",
+"flutter_bootstrap.js": "8d72f12aa0124bc9f86b699f1c944721",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
