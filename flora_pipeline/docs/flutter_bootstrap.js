b40ae7ad68e3ecbbe39338e555907d3c7abc6c68
joindatasolutions-lab <joindatasolutions@gmail.com>
2025-09-24 09:53:19 -0500

    Deploy Flutter Web build

--- flora_pipeline/docs/flutter_bootstrap.js
+++ flora_pipeline/docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3132622317"
+    serviceWorkerVersion: "2716088435"
   }
 });
--- flora_pipeline/docs/flutter_service_worker.js
+++ flora_pipeline/docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f9847c2b9c2b4401d89c1d816d9ed750
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e0d840df47bb6cebba6713bf57500eab",
+"flutter_bootstrap.js": "f9decc48aa66f75fb528b8d07c556c4a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
